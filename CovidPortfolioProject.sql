d62cf02b698b89ab07ff0c4c0a843ebaff4441be
Alice Zeng <106124864+aitingzeng@users.noreply.github.com>
2022-12-29 10:44:09 +1100

    Update and rename CovidProfolioProject.sql to CovidPortfolioProject.sql

--- CovidPortfolioProject.sql
+++ CovidPortfolioProject.sql
@@ -1,16 +1,16 @@
 Select *
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 where continent is not null
 order by 3,4
 
 --Select *
---From ProfolioProject..CovidVaccinations
+--From PortfolioProject..CovidVaccinations
 --order by 3, 4
 
 -- Select Data that we are going to be starting with
 
 Select location, date, total_cases, new_cases, total_deaths, population
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 where continent is not null
 order by 1,2
 
@@ -19,7 +19,7 @@ order by 1,2
 -- Shows likelihood of dying in my country
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 where location like '%australia%'
 and continent is not null
 order by 1,2
@@ -28,7 +28,7 @@ order by 1,2
 -- Shows what percentage of population infected with Covid
 
 Select location, date, population, total_cases, (total_cases/population)*100 as PercenPopulationInfected
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 --where location like '%australia%'
 where continent is not null
 order by 1,2
@@ -36,7 +36,7 @@ order by 1,2
 -- Countries with Highest Infection Rate compared to Population
 
 Select location, population, MAX(total_cases) as HighestInfectionCount, Max(total_cases/population)*100 as PercenPopulationInfected
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 --where location like '%australia%'
 --where continent is not null
 Group by location, population
@@ -45,7 +45,7 @@ order by PercenPopulationInfected desc
 -- Countries with Highest Death Count per Population
 
 Select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 --where location like '%australia%'
 where continent is not null
 Group by continent
@@ -66,7 +66,7 @@ order by TotalDeathCount desc
 -- GLOBAL NUMBERS
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(New_deaths as int))/SUM(new_cases)*100 as DeathPercentage
-From ProfolioProject..CovidDeaths
+From PortfolioProject..CovidDeaths
 --where location like '%australia%'
 where continent is not null
 --group by date
@@ -93,8 +93,8 @@ as
 (
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From ProfolioProject..CovidDeaths dea
-join ProfolioProject..CovidVaccinations vac
+From PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
     On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null
@@ -119,8 +119,8 @@ RollingPeopleVaccinated numeric
 Insert into #PercentPopularVaccinated
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From ProfolioProject..CovidDeaths dea
-join ProfolioProject..CovidVaccinations vac
+From PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
     On dea.location = vac.location
 	and dea.date = vac.date
 --where dea.continent is not null
@@ -135,8 +135,8 @@ From #PercentPopularVaccinated
 Create View PercentPopularVaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From ProfolioProject..CovidDeaths dea
-join ProfolioProject..CovidVaccinations vac
+From PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
     On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null
