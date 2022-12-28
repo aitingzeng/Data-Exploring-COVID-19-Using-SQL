f766a37b159773018e895179a19e7ff11892bf9b
Alice Zeng <106124864+aitingzeng@users.noreply.github.com>
2022-12-28 17:10:22 +1100

    Update CovidProfolioProject.sql

--- CovidProfolioProject.sql
+++ CovidProfolioProject.sql
@@ -7,10 +7,16 @@ order by 3,4
 --From ProfolioProject..CovidVaccinations
 --order by 3, 4
 
+-- Select Data that we are going to be starting with
+
 Select location, date, total_cases, new_cases, total_deaths, population
 From ProfolioProject..CovidDeaths
 where continent is not null
 order by 1,2
+
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying in my country
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From ProfolioProject..CovidDeaths
@@ -18,18 +24,25 @@ where location like '%australia%'
 and continent is not null
 order by 1,2
 
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
 Select location, date, population, total_cases, (total_cases/population)*100 as PercenPopulationInfected
 From ProfolioProject..CovidDeaths
 --where location like '%australia%'
 where continent is not null
 order by 1,2
 
+-- Countries with Highest Infection Rate compared to Population
+
 Select location, population, MAX(total_cases) as HighestInfectionCount, Max(total_cases/population)*100 as PercenPopulationInfected
 From ProfolioProject..CovidDeaths
 --where location like '%australia%'
-where continent is not null
+--where continent is not null
 Group by location, population
 order by PercenPopulationInfected desc
+
+-- Countries with Highest Death Count per Population
 
 Select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 From ProfolioProject..CovidDeaths
@@ -38,7 +51,19 @@ where continent is not null
 Group by continent
 order by TotalDeathCount desc 
 
+-- BREAKING THINGS DOWN BY CONTINENT
 
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+-- GLOBAL NUMBERS
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(New_deaths as int))/SUM(new_cases)*100 as DeathPercentage
 From ProfolioProject..CovidDeaths
@@ -47,7 +72,21 @@ where continent is not null
 --group by date
 order by 1,2
 
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+
+-- Using CTE to perform Calculation on Partition By in previous query
 
 With PopvsVac (Continent, location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
 as
@@ -64,6 +103,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
 
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 DROP Table if exists #PercentPopularVaccinated
 Create Table #PercentPopularVaccinated
@@ -83,13 +123,14 @@ From ProfolioProject..CovidDeaths dea
 join ProfolioProject..CovidVaccinations vac
     On dea.location = vac.location
 	and dea.date = vac.date
-where dea.continent is not null
+--where dea.continent is not null
 --order by 2,3
 
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopularVaccinated
 
 
+-- Creating View to store data for later visualizations
 
 Create View PercentPopularVaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location order by dea.location, dea.Date) as RollingPeopleVaccinated
